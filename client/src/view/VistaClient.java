90b5c0b040b43c6650d84e7f928892218c7dd2c5
Nicole Jimenez <jas.jimenez411@gmail.com>
2019-03-28 17:05:41 +0100

    feat: Create view package
    Create the first View for the Client without the constructor implemented.

---
+++ client/src/view/VistaClient.java
@@ -0,0 +1,22 @@
+package view;
+
+import controller.ControladorBotons;
+import model.Producte;
+
+import javax.swing.*;
+import java.util.LinkedList;
+
+public class VistaClient extends JFrame {
+
+    public VistaClient() {
+
+    }
+
+    void refrescaLlistaCompra(LinkedList<Producte> productes) {
+
+    }
+
+    void assignaControladorBotons(ControladorBotons bController) {
+
+    }
+}
