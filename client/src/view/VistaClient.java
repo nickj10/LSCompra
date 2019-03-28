9c566eab18c41a5dd8449f3916880820ec00044a
Nicole Jimenez <jas.jimenez411@gmail.com>
2019-03-28 18:16:11 +0100

    feat: Add tabbed panes to VistaClient

--- client/src/view/VistaClient.java
+++ client/src/view/VistaClient.java
@@ -8,8 +8,21 @@ import java.util.LinkedList;
 
 public class VistaClient extends JFrame {
 
-    public VistaClient() {
+    private JButton jbAddProduct;
+    private JButton jbRemoveProduct;
+    private JPanel jpMainPanel;
 
+    public VistaClient() {
+        JTabbedPane jtMenu = new JTabbedPane();
+        JPanel jpAddProduct = new JPanel();
+
+        JPanel jpRemoveProduct = new JPanel();
+
+        JPanel jpListProducts = new JPanel();
+
+        jtMenu.addTab("Add Product", jpAddProduct);
+        jtMenu.addTab("Remove Product", jpRemoveProduct);
+        jtMenu.addTab("List products", jpListProducts);
     }
 
     void refrescaLlistaCompra(LinkedList<Producte> productes) {
