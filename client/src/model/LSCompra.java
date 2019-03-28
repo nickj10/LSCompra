c6bd36b4a4651c72cc3a530c8fd1a9f9b5b2f10d
Nicole Jimenez <jas.jimenez411@gmail.com>
2019-03-28 16:28:56 +0100

    feat: Create LSCompra class
    Implement mostraLlistaCompra. Prepare the constructors, setter, getter and the methods.

---
+++ client/src/model/LSCompra.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.LinkedList;
+
+public class LSCompra {
+    private LinkedList<Producte> productes;
+
+    // Constructors
+    public LSCompra(LinkedList<Producte> productes) {
+        this.productes = productes;
+    }
+
+    public LSCompra() {
+        productes = new LinkedList<Producte>();
+    }
+
+    // Getters i Setters
+    public LinkedList<Producte> getProductes() {
+        return productes;
+    }
+
+    public void setProductes(LinkedList<Producte> productes) {
+        this.productes = productes;
+    }
+
+    // Mètodes
+    public void afegeixProducte(Producte producte) {
+
+    }
+
+    public void eliminaProducte(Producte producte) {
+
+    }
+
+    public void mostraLlistaCompra() {
+        System.out.println("Llista de productes de la compra:");
+        for (Producte p: productes) {
+            System.out.println(p);
+        }
+    }
+}
