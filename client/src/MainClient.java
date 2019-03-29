42da8decfa401201f3dee260967de73d1f6a0fe3
Nicole Jimenez <jas.jimenez411@gmail.com>
2019-03-29 17:51:08 +0100

    feat: Create the server communication in MainClient class

--- client/src/MainClient.java
+++ client/src/MainClient.java
@@ -1,5 +1,6 @@
 import controller.ControladorBotons;
 import model.GestorProductes;
+import network.ServerCommunication;
 import view.VistaClient;
 
 public class MainClient {
@@ -10,9 +11,11 @@ public class MainClient {
         // creem el model
         GestorProductes gestorProductes = new GestorProductes();
 
+        // Creem la connexió al servidor
+        ServerCommunication sCommunication = new ServerCommunication(gestorProductes);
 
         // crea el controlador del botons i estableix la relacio C->V i C->M
-        ControladorBotons controladorBotons = new ControladorBotons(gestorProductes, vista);
+        ControladorBotons controladorBotons = new ControladorBotons(gestorProductes, vista, sCommunication);
 
         // establim la relacio V--->C
         vista.assignaControladorBotons(controladorBotons);
