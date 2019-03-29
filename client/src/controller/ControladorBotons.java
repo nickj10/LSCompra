c33d788bee6bb04c93a49b8733c62131739b5c74
Nicole Jimenez <jas.jimenez411@gmail.com>
2019-03-29 17:50:42 +0100

    feat: Add the ServerCommunication to the Controller

--- client/src/controller/ControladorBotons.java
+++ client/src/controller/ControladorBotons.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.GestorProductes;
+import network.ServerCommunication;
 import view.VistaClient;
 
 import java.awt.event.ActionEvent;
@@ -9,10 +10,16 @@ import java.awt.event.ActionListener;
 public class ControladorBotons implements ActionListener {
     private GestorProductes model;
     private VistaClient view;
+    private ServerCommunication serverCommunication;
 
-    public ControladorBotons(GestorProductes model, VistaClient view) {
+    public ControladorBotons(GestorProductes model, VistaClient view, ServerCommunication serverCommunication) {
         this.model = model;
         this.view = view;
+        this.serverCommunication = serverCommunication;
+
+        // Iniciem la recepció d'actualitzacions
+        serverCommunication.startServerComunication();
+        System.out.println("----- CLIENT TO SERVER COM STARTED ----");
     }
 
     @Override
