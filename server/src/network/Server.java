3ca154db9173e4d003b7b8cf4933724b92ed5a53
Nicole Jimenez <jas.jimenez411@gmail.com>
2019-03-29 17:45:17 +0100

    feat: Create Server class
    The Server call extends Thread class so it can start a thread when we start the server in the MainServerThread. It has a list of dedicated servers which are threads as well and are started when a client petition has been accepted by the server through the server socket.
    
    Implement startServer method and stopServer method. These methods put the boolean attribute running as true or false. Instead of writing while(true), we have the option to control the execution of the thread.
    
    When we execute accept(), it will create a socket specifically for that client and it will create a dedicated server. Directly, it starts the thread for that client.

---
+++ server/src/network/Server.java
@@ -0,0 +1,115 @@
+package network;
+
+import model.GestorProductes;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+public class Server extends Thread {
+    private String IP;
+    private int port;
+    private boolean running;
+    private ServerSocket serverSocket;
+    private GestorProductes model;
+    private LinkedList<DedicatedServer> dedicatedServers;
+
+
+    public Server(GestorProductes model) {
+        try {
+            this.running = false;
+            this.model = model;
+            // Obrim un socket per al servidor
+            this.serverSocket = new ServerSocket(10069);
+            this.dedicatedServers = new LinkedList<DedicatedServer>();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startServer() {
+        // Iniciem el thread del servidor
+        running = true;
+        this.start();
+    }
+
+    public void stopServer() {
+        // Aturem el thread del servidor
+        running = false;
+        this.interrupt();
+    }
+
+    public void run()  {
+        while (running) {
+            try {
+                // Acceptem peticions de connexio dels clients
+                Socket socketClient = serverSocket.accept();
+
+                // Creem un nou servidor dedicat per atendre les peticions del client
+                DedicatedServer client = new DedicatedServer(socketClient, dedicatedServers, model, this);
+                dedicatedServers.add(client);
+
+
+                System.out.println("---- Peticion aceptada ---");
+                // Arrencar el servidor dedicat
+                client.startDedicatedServer();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // Aturem tots els servidors dedicats creats quan ja no atenem més peticions
+        for (DedicatedServer d : dedicatedServers) {
+            d.startDedicatedServer();
+        }
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+    public void setServerSocket(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public GestorProductes getModel() {
+        return model;
+    }
+
+    public void setModel(GestorProductes model) {
+        this.model = model;
+    }
+
+    public LinkedList<DedicatedServer> getDedicatedServers() {
+        return dedicatedServers;
+    }
+
+    public void setDedicatedServers(LinkedList<DedicatedServer> dedicatedServers) {
+        this.dedicatedServers = dedicatedServers;
+    }
+}
