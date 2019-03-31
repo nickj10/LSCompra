f9a606b24b694764e53bb651a9d00357b8f0036c
Nicole Jimenez <jas.jimenez411@gmail.com>
2019-04-01 00:04:50 +0200

    fix: Change the ip from localhost to 127.0.0.1

--- client/src/network/ServerConfiguration.java
+++ client/src/network/ServerConfiguration.java
@@ -1,6 +1,6 @@
 package network;
 
 public class ServerConfiguration {
-    public static final String SERVER_IP = "localhost";
+    public static final String SERVER_IP = "127.0.0.1";
     public static final int SERVER_PORT = 10069;
 }
